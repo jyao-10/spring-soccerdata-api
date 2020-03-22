24c1ed16ffd495a91547960c96b70ee72725d19f
jyao-10 <jiayuyao@Jiayus-MacBook-Pro.local>
2020-03-22 00:43:29 -0700

    initial commit

---
+++ src/main/java/com/SoccerAPI/SoccerApiApplication.java
@@ -0,0 +1,13 @@
+package com.SoccerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SoccerAPI/SoccerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SoccerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
